60a9d042ac4d218a152d18c9c8533fb4b0300b03
osho81 <ossi.hour@gmail.com>
2023-06-17 14:38:06 +0200

    Update comments

--- src/components/ListAllCarsComponent.js
+++ src/components/ListAllCarsComponent.js
@@ -6,7 +6,8 @@ import { useNavigate } from 'react-router-dom';
 
 import LoadingSpinnerComponent from './LoadingSpinnerComponent';
 
-// Keycloak imports (also see npm install of tehese)
+// Keycloak imports (also see npm install of these)
+import Keycloak from 'keycloak-js';
 import { useKeycloak } from '@react-keycloak/web'
 
 // Fontawsome for react; combine into an element before usage
